474aa5b2110169b4d74e5cff087646ac82033bc9
Anouk Ober <anouk.ober@unice.fr>
2026-06-04 11:02:48 +0200

    essai de creation de la page web UI clean ( assets ici mais non utilisé)

--- src/main/java/com/petitbac/petitbac_v2/PetitbacV2Application.java
+++ src/main/java/com/petitbac/petitbac_v2/PetitbacV2Application.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class PetitbacV2Application {
 
-	public static void main(String[] args) {
+	public 	static void main(String[] args) {
 		SpringApplication.run(PetitbacV2Application.class, args);
 	}
 
